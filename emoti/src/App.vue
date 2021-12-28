a20d0d65ff7b7d3f2ef5c6c807b1ad906005ce9b
mmalta24 <marcoantoniodacostamalta24@gmail.com>
2021-12-28 15:04:42 +0000

    'set first'

---
+++ emoti/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div id="nav" v-if="trueorfalse == false">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      trueorfalse: true,
+    };
+  },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: Ambit;
+  src: "../font/Ambit-Regular.ttf";
+}
+#app {
+  font-family: Ambit;
+  user-select: none;
+}
+</style>
---
+++ emoti/src/views/Home.vue
@@ -0,0 +1,137 @@
+<template>
+  <div id="background" class="d-flex align-items-center">
+    <b-container class="col-12 d-flex flex-row justify-content-between pl-5 pr-2">
+      <div class="col-4 d-flex flex-column align-items-center justify-content-around pt-2">
+        <img src="../assets/logo2.svg" width="400px" alt="" />
+        
+        <div class="mt-3 text-center">
+          <h5 style="font-size: 25px">
+            Olá queres brincar e aprender ao mesmo tempo ?
+          </h5>
+          <p style="font-size: 20px">
+            Não te preocupes! Estás no sítio certo,para além disso, os teus pais
+            e professores podem juntarem-se a ti, nesta aventura!
+          </p>
+        </div>
+        
+        <div class="w-75 d-flex flex-row justify-content-around pt-2">
+          <b-button @click="whatDo = 'login'" size="lg" style="width: 40%;background-color: #e87461;border: #e87461;font-weight: 600;" v-b-modal.modal-center>Entrar</b-button>
+          <b-button @click="whatDo = 'register'" size="lg" style="width: 40%;background-color: #fdfdf3;border: 1px solid #e87461;color: #e87461;font-weight: 600;" v-b-modal.modal-center>Registar</b-button>
+        </div>
+
+      </div>
+
+      <div class="col-7">
+        <img src="../assets/foto_capa.png" width="720px" alt="" />
+      </div>
+
+      <b-modal id="modal-center" centered hide-footer header-border-variant="0" header-class="color" body-class="color">
+        <div v-if="whatDo == 'login'" style="font-family: Ambit;color:#e87461" class="text-center">
+           <h3 class="mt-2 mb-4">Autenticar</h3>
+
+          <b-form action="">
+
+            <b-input-group class="mb-4">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">badge</span>
+              </b-input-group-prepend>
+              <b-form-input v-model="text" placeholder="Username" style="background-color: #fdfdf3" required></b-form-input>
+            </b-input-group>
+
+            <b-input-group class="mb-4">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">lock</span>
+              </b-input-group-prepend>
+              <b-form-input type="password" v-model="text" placeholder="Password" style="background-color: #fdfdf3" required></b-form-input>
+            </b-input-group>
+
+            <div class="d-flex flex-row justify-content-end">
+              <b-button type="submit" style="background-color: #e87461; color: #fdfdf3; border: none" class="mb-2 w-25">Entrar</b-button>
+            </div>
+
+          </b-form>
+        </div>
+
+        <div v-else style="font-family: Ambit; color: #e87461" class="text-center">
+          <h3 class="mt-3 mb-4">Registar</h3>
+
+          <b-form action="">
+            <b-input-group class="mb-4">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">badge</span>
+              </b-input-group-prepend>
+              <b-form-input v-model="text" placeholder="Username" style="background-color: #fdfdf3" required></b-form-input>
+            </b-input-group>
+
+            <b-input-group class="mb-4">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">lock</span>
+              </b-input-group-prepend>
+              <b-form-input type="password" v-model="text" placeholder="Password" style="background-color: #fdfdf3" required></b-form-input>
+            </b-input-group>
+
+            <b-input-group class="mb-4">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">lock</span>
+              </b-input-group-prepend>
+              <b-form-input type="password" v-model="text" placeholder="Confirmar Password" style="background-color: #fdfdf3" required></b-form-input>
+            </b-input-group>
+
+            <b-input-group class="mb-4">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">person</span>
+              </b-input-group-prepend>
+              <b-form-input v-model="text" placeholder="Nome" style="background-color: #fdfdf3" required></b-form-input>
+            </b-input-group>
+
+            <b-input-group class="mb-4">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">send</span>
+              </b-input-group-prepend>
+              <b-form-input type="email" v-model="text" placeholder="Email" style="background-color: #fdfdf3" required></b-form-input>
+            </b-input-group>
+
+            <b-input-group class="mb-5">
+              <b-input-group-prepend is-text>
+                <span class="material-icons-round">person_search</span>
+              </b-input-group-prepend>
+              <b-form-select v-model="selected" required style="background-color: #fdfdf3">
+                <b-form-select-option :value="null" disabled>Quem sou eu ?</b-form-select-option>
+                <b-form-select-option v-for="(option, index) in options" :key="index" :value="option">{{ option }}</b-form-select-option>
+              </b-form-select>
+            </b-input-group>
+
+            <div class="d-flex flex-row justify-content-end">
+              <b-button type="submit" style="background-color: #e87461; color: #fdfdf3; border: none" class="mb-2 w-25">Registar</b-button>
+            </div>
+          </b-form>
+        </div>
+      </b-modal>
+      
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      whatDo: "",
+      options: ["Criança", "Tutor", "Professor"],
+      selected: null,
+    };
+  },
+};
+</script>
+
+<style>
+#background {
+  width: 100vw;
+  height: 100vh;
+  background: url("../assets/wallpaper1.svg");
+  background-size: 1540px auto;
+}
+.color{
+  background-color:#fdfdf3
+}
+</style>
