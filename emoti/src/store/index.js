010a6dec61ee445559e76e48808cc003f257f440
Joao Santos <40200305@esmad.ipp.pt>
2021-12-29 15:33:49 +0000

    store

--- emoti/src/store/index.js
+++ emoti/src/store/index.js
@@ -4,8 +4,49 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    users: localStorage.users
+      ? JSON.parse(localStorage.users)
+      : [
+          {
+            username: "admin",
+            password: "admin",
+            type: "admin",
+          },
+        ],
+    loggedUser: null,
+  },
+  
+  getters: {
+    isUser: (state) => (username, password) =>
+      state.users.some(
+        (user) => user.username === username && user.password === password
+      ),
+
+    isUsernameAvailable: (state) => (username) =>
+      state.users.every((user) => user.username !== username),
+
+    getLoggedUser: (state) => state.loggedUser,
+
+  },
+
+  mutations: {
+    SET_LOGGED_USER(state, variable) {
+      state.loggedUser = state.users.find((user) => user.username === variable);
+      localStorage.loggedUser = JSON.stringify(state.loggedUser);
+    },
+
+    SET_NEW_USER(state, variable) {
+      state.users.push(variable);
+      localStorage.users = JSON.stringify(state.users);
+    },
+
+    SET_LOGOUT(state) {
+      state.loggedUser = null;
+      localStorage.removeItem("loggedUser");
+    },
+
+  },
+
   modules: {},
 });
